e2b8c20521b1b17036afbf0d372acec68bbb2ac3
momunbaevam <93367394+momunbaevam@users.noreply.github.com>
2021-11-05 12:03:12 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 <h1>Mathematical Symbol Example</h1>
 
 <h2>The for-all symbol: &forall;</h2>
+  hello, this is my first web site
 
 </body>
 </html>
+ <h1 style="color:Tomato;">Hello World</h1>
+<p style="color:DodgerBlue;">Lorem ipsum...</p>
+<p style="color:MediumSeaGreen;">Ut wisi enim...</p> 
